3f77062bff7a1bccc4bf29d50e4061753369a7a6
kolamat <matanmidupe004@gmail.com>
2024-08-20 16:15:50 +0100

    On branch main Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   index.css
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const body = document.querySelector("body");
 body.style.margin = "0px";
+body.style.backgroundColor = "#aae92c;";
 
 const container = document.createElement("div");
 container.id = "div";
 container.style.display = "grid";
-container.style.gridTemplateRows = "200px 200px";
-container.style.gridTemplateColumns = "200px";
+container.style.textAlign = "center";
+container.style.justifyContent = "center";
 document.querySelector("body").appendChild(container);
 
 const H1 = document.createElement("h1");
@@ -15,26 +16,39 @@ document.getElementById("div").appendChild(H1);
 const imgContainer = document.createElement("div");
 imgContainer.id = "imgDiv";
 imgContainer.style.display = "grid";
-imgContainer.style.gridTemplateRows = "200px 200px";
+imgContainer.style.gridTemplateRows = "300px";
 imgContainer.style.gridTemplateColumns = "200px 200px";
 document.getElementById("div").appendChild(imgContainer);
 
 const img1 = document.createElement("img");
 img1.src = "/image1.jpg";
-img1.style.width = "100px";
+img1.style.width = "200px";
 document.getElementById("imgDiv").appendChild(img1);
 
 const img2 = document.createElement("img");
 img2.src = "/image2.jpg";
-img2.style.width = "100px";
+img2.style.marginTop = "100px";
+img2.style.width = "200px";
 document.getElementById("imgDiv").appendChild(img2);
 
 const img3 = document.createElement("img");
 img3.src = "/image3.jpg";
-img3.style.width = "100px";
-img3.style.columnSpan = "all";
-// img3.style.gridRowStart = "1";
-// img3.style.gridRowEnd = "2";
-// img3.style.gridColumnStart = "1";
-// img3.style.gridColumnEnd = "3";
+img3.style.width = "200px";
+img3.style.height = "full";
+img3.style.gridAutoRows = "2";
 document.getElementById("imgDiv").appendChild(img3);
+
+const button = document.createElement("button");
+button.textContent = "Click-Me";
+button.style.padding = "20px";
+button.style.width = "100px";
+button.style.borderRadius = "30px";
+button.style.marginTop = "30px";
+button.onclick = document.getElementById("div").appendChild(button);
+
+function smile() {
+  const p = document.createElement("p");
+  p.textContent =
+    "🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣";
+  document.querySelector("button").appendChild(p);
+}
