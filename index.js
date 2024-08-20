0ac3848d00b8df987e2b4e33dd51afe438071424
kolamat <matanmidupe004@gmail.com>
2024-08-20 15:17:02 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	new file:   index.css
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const container = document.createElement("div");
+container.textContent = "am a container";
+container.id = "div";
+container.style.display = "grid";
+container.style.gridTemplateRows = "200px 200px";
+container.style.gridTemplateColumns = "200px 200px";
+document.querySelector("body").appendChild(container);
+
 const H1 = document.createElement("h1");
 H1.textContent = "Hi Love";
-document.querySelector("body").appendChild(H1);
+document.getElementById("div").appendChild(H1);
+
+const img1 = document.createElement("img");
+img1.src = "/image1.jpg";
+img1.style.width = "100px";
+document.getElementById("div").appendChild(img1);
+
+const img2 = document.createElement("img");
+img2.src = "/image2.jpg";
+img2.style.width = "100px";
+document.getElementById("div").appendChild(img2);
+
+const img3 = document.createElement("img");
+img3.src = "/image3.jpg";
+img3.style.width = "100px";
+img3.style.columnSpan = "span 2";
+img3.style.gridRowEnd = "2";
+document.getElementById("div").appendChild(img3);
