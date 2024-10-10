72208dca0663759675198c088da157d448c9331d
kolamat <matanmidupe004@gmail.com>
2024-10-10 01:01:05 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-const shuffledImage = [
-  { image1: "images/image1.jpg" },
-  { image2: "images/image2.jpg" },
-  { image3: "images/image3.jpg" },
-  { image4: "images/image4.JPG" },
-  { image5: "images/image5.JPG" },
-  { image6: "images/image6.JPG" },
-  { image7: "images/image7.JPG" },
-  { image8: "images/image8.JPG" },
-  { image9: "images/image9.JPG" },
-  { image10: "images/image10.JPG" },
-  { image11: "images/image11.JPG" },
-  { image12: "images/image12.JPG" },
-  { image13: "images/image13.JPG" },
-];
-
-console.log(shuffledImage);
-
 const shuffledVideo = ["vid1.mp4", "vid2.mp4", "vid3.mp4"];
 
 const body = document.querySelector("body");
@@ -30,36 +12,37 @@ container.style.height = "100vh";
 container.style.justifyContent = "center";
 document.querySelector("body").appendChild(container);
 
-const H1 = document.createElement("h1");
-H1.textContent = "Hi Love";
+const H1 = document.createElement("h3");
+H1.textContent = "Hi Love, My Mighty High Damsel";
 document.getElementById("div").appendChild(H1);
 
 const imgContainer = document.createElement("div");
 imgContainer.id = "imgDiv";
 imgContainer.style.display = "grid";
-imgContainer.style.gridTemplateRows = "300px";
+imgContainer.style.gridTemplateRows = "2 00px";
 imgContainer.style.gridTemplateColumns = "200px 200px";
 imgContainer.style.gap = "10px";
 document.getElementById("div").appendChild(imgContainer);
 
 const img1 = document.createElement("img");
 img1.src = "images/image1.jpg";
-img1.style.width = "180px";
+img1.style.width = "200px";
+img1.style.height = "200px";
 img1.style.borderRadius = "30px";
-img1.style.marginLeft = "20px";
+img1.style.borderRadius = "30px";
 document.getElementById("imgDiv").appendChild(img1);
 
 const img2 = document.createElement("img");
 img2.src = "images/image2.jpg";
-img2.style.marginTop = "100px";
-img2.style.width = "180px";
+img2.style.width = "200px";
+img2.style.height = "200px";
 img2.style.borderRadius = "30px";
 document.getElementById("imgDiv").appendChild(img2);
 
 const img3 = document.createElement("img");
 img3.src = "images/image3.jpg";
-img3.style.width = "180px";
-img3.style.marginLeft = "20px";
+img3.style.width = "200px";
+img3.style.height = "200px";
 img3.style.borderRadius = "30px";
 document.getElementById("imgDiv").appendChild(img3);
 
@@ -85,70 +68,75 @@ button.style.color = "white";
 button.addEventListener("click", smile);
 document.getElementById("buttoncontainer").appendChild(button);
 
-const imageA = document.createElement("img");
-imageA.src = "images/image1.jpg";
-imageA.style.width = "180px";
-document.querySelector("body").appendChild(imageA);
-
-const imageB = document.createElement("img");
-imageB.src = "images/image2.jpg";
-imageB.style.width = "180px";
-document.querySelector("body").appendChild(imageB);
-
-const imageC = document.createElement("img");
-imageC.src = "images/image3.jpg";
-imageC.style.width = "180px";
-document.querySelector("body").appendChild(imageC);
-
 const imageD = document.createElement("img");
 imageD.src = "images/image4.JPG";
-imageD.style.width = "180px";
-document.querySelector("body").appendChild(imageD);
+imageD.style.width = "200px";
+imageD.style.borderRadius = "10px";
+imageD.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageD);
 
 const imageE = document.createElement("img");
 imageE.src = "images/image5.JPG";
-imageE.style.width = "180px";
-document.querySelector("body").appendChild(imageE);
+imageE.style.width = "200px";
+imageE.style.borderRadius = "10px";
+imageE.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageE);
 
 const imageF = document.createElement("img");
 imageF.src = "images/image6.JPG";
-imageF.style.width = "180px";
-document.querySelector("body").appendChild(imageF);
+imageF.style.width = "200px";
+imageF.style.borderRadius = "10px";
+imageF.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageF);
 
 const imageG = document.createElement("img");
 imageG.src = "images/image7.JPG";
-imageG.style.width = "180px";
-document.querySelector("body").appendChild(imageG);
+imageG.style.width = "200px";
+imageG.style.borderRadius = "10px";
+imageG.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageG);
 
 const imageH = document.createElement("img");
 imageH.src = "images/image8.JPG";
-imageH.style.width = "180px";
-document.querySelector("body").appendChild(imageH);
+imageH.style.width = "200px";
+imageH.style.borderRadius = "10px";
+imageH.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageH);
 
 const imageI = document.createElement("img");
 imageI.src = "images/image9.JPG";
-imageI.style.width = "180px";
-document.querySelector("body").appendChild(imageI);
+imageI.style.width = "200px";
+imageI.style.borderRadius = "10px";
+imageI.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageI);
 
 const imageJ = document.createElement("img");
 imageJ.src = "images/image10.JPG";
-imageJ.style.width = "180px";
-document.querySelector("body").appendChild(imageJ);
+imageJ.style.width = "200px";
+imageJ.style.borderRadius = "10px";
+imageJ.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageJ);
 
 const imageK = document.createElement("img");
 imageK.src = "images/image11.JPG";
-imageK.style.width = "180px";
-document.querySelector("body").appendChild(imageK);
+imageK.style.width = "200px";
+imageK.style.borderRadius = "10px";
+imageK.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageK);
 
 const imageL = document.createElement("img");
 imageL.src = "images/image12.JPG";
-imageL.style.width = "180px";
-document.querySelector("body").appendChild(imageL);
+imageL.style.width = "200px";
+imageL.style.borderRadius = "10px";
+imageL.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageL);
 
 const imageM = document.createElement("img");
 imageM.src = "images/image13.JPG";
-imageM.style.width = "180px";
-document.querySelector("body").appendChild(imageM);
+imageM.style.width = "200px";
+imageM.style.borderRadius = "10px";
+imageM.style.height = "200px";
+document.getElementById("imgDiv").appendChild(imageM);
 
 function smile() {
   alert(
