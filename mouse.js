8e8acaafa366b54b387043bb812d84c622db967a
kolamat <matanmidupe004@gmail.com>
2024-10-22 21:34:10 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   mouse.css
    	modified:   mouse.html
    	new file:   mouse.js

---
+++ mouse.js
@@ -0,0 +1,11 @@
+const cursor = document.querySelector(".cursor");
+
+//follow cursor on mousemove
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+});
